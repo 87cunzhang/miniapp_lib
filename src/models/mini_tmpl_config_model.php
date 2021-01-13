80f64f9d8bd7aa8f597e7d80d34b6bfbd1540924
87cunzhang <2423561050@qq.com>
2021-01-13 16:30:51 +0800

    移动model文件

---
+++ src/models/mini_tmpl_config_model.php
@@ -0,0 +1,19 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Mini_tmpl_config_model extends MY_Model
+{
+    protected $db_group_name = 'member';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->table = 'txzj_mini_tmpl_config';
+    }
+
+    public function get_mini_tmpl_type()
+    {
+        $this->database->select();
+        $this->database->group_by('tmpl_type');
+        return $this->database->get($this->table)->result_array();
+    }
+}
---
+++ src/models/mini_tmpl_history_model.php
@@ -0,0 +1,21 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Mini_tmpl_history_model extends MY_Model
+{
+    protected $db_group_name = 'member';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->table = 'txzj_mini_tmpl_history';
+    }
+
+    public function get_last_history($where)
+    {
+        $this->database->select();
+        $this->database->where($where);
+        $this->database->order_by('id', 'DESC');
+        $this->database->limit(1, 0);
+        return $this->database->get($this->table)->row_array();
+    }
+}
